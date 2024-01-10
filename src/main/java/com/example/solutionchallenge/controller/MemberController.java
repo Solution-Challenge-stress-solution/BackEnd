2eeb57a6307e285032b59aae2509dc349e76550d
고경남 <rhrudska987@naver.com>
2024-01-10 17:34:55 +0900

    feat: yml파일 추가

--- src/main/java/com/example/solutionchallenge/controller/MemberController.java
+++ src/main/java/com/example/solutionchallenge/controller/MemberController.java
@@ -4,10 +4,12 @@ import com.example.solutionchallenge.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/")
 public class MemberController {
 
     private final MemberService memberService;
