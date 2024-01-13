ab6162ab2a309753d0be8ffda59389771d892d76
prislewarz <whdwns5060@naver.com>
2024-01-13 17:11:29 +0900

    fix: 멤버 관련 폴더 삭제(User 리팩토링 예정), feat: 파이어베이스 카카오 로그인 기능 추가

---
+++ src/main/java/com/example/solutionchallenge/common/config/FirebaseConfig.java
@@ -0,0 +1,34 @@
+package com.example.solutionchallenge.common.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/loginsecret.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                System.out.println("Firebase application has been initialized");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
--- src/main/java/com/example/solutionchallenge/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.solutionchallenge.repository;
-
-import com.example.solutionchallenge.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
