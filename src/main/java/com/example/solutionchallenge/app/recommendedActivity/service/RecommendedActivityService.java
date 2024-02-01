ebfff6cbf46f4321053f6e05fa74dfd181240bf6
고경남 <rhrudska987@naver.com>
2024-02-01 17:22:16 +0900

    feat: make activity fuctions

---
+++ src/main/java/com/example/solutionchallenge/app/recommendedActivity/service/RecommendedActivityService.java
@@ -0,0 +1,23 @@
+package com.example.solutionchallenge.app.recommendedActivity.service;
+
+import com.example.solutionchallenge.app.recommendedActivity.domain.RecommendedActivity;
+import com.example.solutionchallenge.app.recommendedActivity.dto.ActivityResponseDto;
+import com.example.solutionchallenge.app.recommendedActivity.repository.RecommendedActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class RecommendedActivityService {
+
+    private final RecommendedActivityRepository recommendedActivityRepository;
+
+    public ActivityResponseDto findById(Long activityId) {
+        RecommendedActivity recommendedActivity = recommendedActivityRepository.findById(activityId).orElseThrow(
+                () -> new IllegalArgumentException("해당 추천 활동이 없습니다. id=" + activityId));
+        ActivityResponseDto activityResponseDto = ActivityResponseDto.builder().recommendedActivity(recommendedActivity).build();
+        return activityResponseDto;
+    }
+}
