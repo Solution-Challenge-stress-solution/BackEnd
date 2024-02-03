a6dbbc5b8d1b57bf266d4e04a953603cbfb90420
orieasy1 <easy1nhard2@gmail.com>
2024-02-04 06:13:33 +0900

    망한듯

--- src/main/java/com/example/solutionchallenge/app/auth/controller/OauthController.java
+++ src/main/java/com/example/solutionchallenge/app/auth/controller/OauthController.java
@@ -1,17 +1,13 @@
 package com.example.solutionchallenge.app.auth.controller;
 
+import com.example.solutionchallenge.app.auth.dto.request.OauthRequestDto;
+import com.example.solutionchallenge.app.auth.dto.response.OauthResponseDto;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OauthController {
 
-    @GetMapping("/login/oauth/google")
-    public String login(@AuthenticationPrincipal OAuth2User principal) {
-        String email = principal.getAttribute("email");
-        System.out.println("User email: " + email);
-        return "로그인 성공! 사용자 이메일: " + email;
-    }
 }
