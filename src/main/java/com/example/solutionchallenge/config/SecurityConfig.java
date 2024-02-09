d6b01748ee783ec25d895571db8a280698a3c3cd
prislewarz <whdwns5060@naver.com>
2024-02-09 16:15:13 +0900

    refactor: modify SecurityConfig

--- src/main/java/com/example/solutionchallenge/config/SecurityConfig.java
+++ src/main/java/com/example/solutionchallenge/config/SecurityConfig.java
@@ -1,80 +1,39 @@
 package com.example.solutionchallenge.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         http
-                .httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .anyRequest().authenticated();
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeRequest) -> authorizeRequest.anyRequest().permitAll());
+//                        .requestMatchers(new MvcRequestMatcher(introspector, "")).permitAll())
+//                .oauth2Login(oauth2Login ->
+//                        oauth2Login
+//                                .successHandler()
+//                                .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService)));
+
+        return http.build();
     }
 }
-
-//
-//import com.example.solutionchallenge.app.handler.ExceptionHandlerFilter;
-//import com.example.solutionchallenge.app.user.domain.UserRole;
-//import com.example.solutionchallenge.app.auth.domain.jwt.JwtFilter;
-//import com.example.solutionchallenge.app.auth.domain.jwt.JwtTokenService;
-//import com.example.solutionchallenge.app.user.service.UserService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@RequiredArgsConstructor
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//    private final JwtTokenService jwtTokenService;
-//    private final UserService userService;
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(
-//            final AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
-//        return http.cors(withDefaults())
-//                .csrf((csrf) -> csrf.disable())
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers(new AntPathRequestMatcher("/login/**"), new AntPathRequestMatcher("/token/refresh")).permitAll()
-//                        .requestMatchers(new AntPathRequestMatcher("/user/**")).hasAuthority(UserRole.USER.getRole())
-//                        .anyRequest().authenticated())
-//                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable()) // 로그인 폼 미사용
-//                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable()) // http basic 미사용
-//                .addFilterBefore(new JwtFilter(jwtTokenService, userService), UsernamePasswordAuthenticationFilter.class) // JWT Filter 추가
-//                .addFilterBefore(new ExceptionHandlerFilter(), JwtFilter.class) // Security Filter 에서 CustomException 사용하기 위해 추가
-//                .build();
-//    }
-//
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer(){
-//        // 아래 url은 filter 에서 제외
-//        return web ->
-//                web.ignoring()
-//                        .requestMatchers(new AntPathRequestMatcher("/login/**"), new AntPathRequestMatcher("/token/refresh"));
-//    }
-//}
